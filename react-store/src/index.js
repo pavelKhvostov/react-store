78c9b3f660d7fd22e5ebf9ab2a3a2feed957826d
pavelKhvostov <137449802+pavelKhvostov@users.noreply.github.com>
2025-03-31 19:49:54 +0300

    newVersionMy

--- react-store/src/index.js
+++ react-store/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-import './index.scss';
-import 'macro-css';
+import './scss/index.scss';
 
 import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
--- react-store/src/pages/Favorites.jsx
+++ react-store/src/pages/Favorites.jsx
@@ -1,19 +1,5 @@
-import Card from '../components/Card';
-
-function Favorites({ items, onAddToFavorite }) {
-  return (
-    <div className="content p-40">
-      <div className="d-flex align-center justify-between mb-40">
-        <h1>Мои закладки</h1>
-      </div>
-
-      <div className="d-flex flex-wrap">
-        {items.map((item, index) => (
-          <Card key={index} favorited={true} onFavorite={onAddToFavorite} {...item} />
-        ))}
-      </div>
-    </div>
-  );
-}
+const Favorites = () => {
+  return <div className='content'></div>;
+};
 
 export default Favorites;
